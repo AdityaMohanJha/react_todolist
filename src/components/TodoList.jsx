32af3356f367200d2ebb3dc0f7d13a8e13ef014f
AdityaMohanJha <149247310+AdityaMohanJha@users.noreply.github.com>
2026-02-15 17:45:19 +0530

    Add files via upload

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import TodoCard from "./todoCard"
+
+export default function TodoList(props) {
+  //to destructure out todos
+  const {todos} = props
+  
+  
+  
+    return (
+    <ul className='main'>
+        {todos.map((todo, todoindex)  => {
+            return(
+               <TodoCard {...props} key={todoindex} index =
+               {todoindex}>
+                <p>{todo}</p>
+               </TodoCard>
+            )
+        })}
+    </ul>
+  )
+}
